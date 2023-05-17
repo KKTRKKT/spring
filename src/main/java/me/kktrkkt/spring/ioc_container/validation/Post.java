463281976b34c1fa902fbb375300c4de9f4ab350
KKTRKKT <paulhana6006@gmail.com>
2023-05-17 10:36:22 +0900

    validatoin 예제 추가

---
+++ src/main/java/me/kktrkkt/spring/ioc_container/validation/Post.java
@@ -0,0 +1,37 @@
+package me.kktrkkt.spring.ioc_container.validation;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public class Post {
+
+    private Integer id;
+
+    @NotEmpty
+    private String title;
+
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
